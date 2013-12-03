2cb4becc0adce6a9fe4dc3f1d9290a79d542a5e2
Martin A. Midtsund <martin@iterate.no>
2013-12-03 12:04:32 +0100

    Add default admin-user if no users exist

--- server/main.server.js
+++ server/main.server.js
@@ -1,5 +1,21 @@
-if(Meteor.isServer) {
-    Accounts.validateNewUser(function (user) {
-        throw new Meteor.Error(403, "You are not allowed to create new users");
-    });
-}
+// Server only logic, this will NOT be sent to the clients.
+
+Meteor.startup(function () {
+    //TODO(martin): This needs to be set to a username and password agreed with Red Cross
+    if(Meteor.users.find().count() === 0) {
+        console.log("WARNING: NO USERS, DEFAULT ADMIN ACCOUNT ADDED");
+        var options = {
+            username: 'admin',
+            password: 'admin',
+            role: ROLES.admin,
+            email: 'martin@iterate.no'
+        };
+        Accounts.createUser(options);
+    };
+});
+
+Accounts.validateNewUser(function (user) {
+    //TODO(martin): Should be able to create users on server, not on client.
+    //throw new Meteor.Error(403, "You are not allowed to create new users");
+    return true;
+});
