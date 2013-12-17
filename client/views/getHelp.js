e396dfc483eb3b75682ea9f613d6b3c1c13ed2d2
Martin A. Midtsund <martin@iterate.no>
2013-12-17 12:24:16 +0100

    Add modal for students with queue information

--- client/views/getHelp.js
+++ client/views/getHelp.js
@@ -10,75 +10,44 @@ var getHighestQueueNr = function () {
     return StudentQueue.find({}, { sort: { queueNr: -1 }, limit: 1 }).fetch()[0].queueNr;
 };
 
-var videoConferenceDep = new Deps.Dependency();
-var videoConferenceUrl;
-
 // === GETHELP ===
-Template.getHelp.videoConferenceUrl = function () {
-    videoConferenceDep.depend();
-    return videoConferenceUrl;
-};
+var validationErrorDep = new Deps.Dependency;
+var validationError;
 
-Template.getHelp.studentSession = function () {
-    return StudentSessions.find({ _id: Session.get("studentSessionId") }).fetch()[0];
-};
-
-Template.getHelp.stateReady = function () {
-    return Template.getHelp.studentSession().state == STUDENT_SESSION_STATE.READY;
-};
-
-Template.getHelp.stateWaiting = function () {
-    return Template.getHelp.studentSession().state == STUDENT_SESSION_STATE.WAITING;
-};
-
-// TODO(martin): Rewrite to use Dependency? Is it needed?
-// See http://docs.meteor.com/#deps
-// and http://docs.meteor.com/#meteor_publish
-Template.getHelp.queueSize = function () {
-    var queueSize = StudentQueue.find({}).count();
-    if (!queueSize) {
-        return "";
-    } else {
-        return queueSize;
-    }
-};
-
-Template.getHelp.studentsInFront = function () {
-    var studentsInFront = StudentQueue.find({ queueNr: { $lt: Session.get("queueNr") }}).count();
-    return studentsInFront;
+Template.getHelp.validationError = function () {
+    validationErrorDep.depend();
+    return validationError;
 };
 
 Template.getHelp.events({
-    'click button#getHelp' : function () {
-        window.open(this.videoConferenceUrl);
-        StudentSessions.update(
-            { _id: Session.get("studentSessionId") },
-            { $set: { state: STUDENT_SESSION_STATE.GETTING_HELP } });
-    },
-
     'click button#createSession' : function () {
-        videoConferenceUrl = generateRandomAppearInLink();
-        videoConferenceDep.changed();
-        Session.set("videoConferenceUrl", videoConferenceUrl);
+        Session.set("videoConferenceUrl", generateRandomAppearInLink());
         var queueNr = getHighestQueueNr() + 1;
-        console.log("queueNr = " + queueNr);
-        console.log("subject = " + $('#chosenSubject').text());
-        console.log("grade = " + $('#chosenGrade').text());
-        StudentSessions.insert({
-            subject: $('#chosenSubject').text(),
-            grade: $('#chosenGrade').text(),
-            // topic: $('#topic').val(),
-            videoConferenceUrl: videoConferenceUrl,
-            state: STUDENT_SESSION_STATE.WAITING,
-            queueNr: queueNr
-        }, function (error, id) {
-            if (error) { return null; };
-            Session.set("studentSessionId", id);
-            Session.set("queueNr", queueNr);
-        });
-        $('#subject').val("");
-        $('#grade').val("");
-        $('#topic').val("");
+        var chosenSubject = $('#chosenSubject').text().trim();
+        var chosenGrade = $('#chosenGrade').text().trim();
+
+        if (chosenSubject === "Velg ønsket fag") {
+            validationError = "Feil: Du må velge et fag";
+            validationErrorDep.changed();
+        } else if (chosenGrade === "Velg ditt trinn") {
+            validationError = "Feil: Du må velge et trinn";
+            validationErrorDep.changed();
+        } else {
+            validationError = null;
+            validationErrorDep.changed();
+            StudentSessions.insert({
+                subject: $('#chosenSubject').text(),
+                grade: $('#chosenGrade').text(),
+                videoConferenceUrl: Session.get("videoConferenceUrl"),
+                state: STUDENT_SESSION_STATE.WAITING,
+                queueNr: queueNr
+            }, function (error, id) {
+                if (error) { return null; };
+                Session.set("studentSessionId", id);
+                Session.set("queueNr", queueNr);
+                $('#queueModal').modal();
+            });
+        }
     }
 });
 
---
+++ client/views/queueModal.js
@@ -0,0 +1,38 @@
+Template.queueModal.events({
+    'click button#leaveQueue' : function () {
+        StudentSessions.remove({ _id: Session.get("studentSessionId") });
+        return false;
+    }
+});
+
+Template.queueModal.rendered = function () {
+    $('#queueModal').on('hide.bs.modal', function () {
+        $('#queueModal').off('hide.bs.modal');
+    })
+}
+
+Template.queueModalBody.studentSession = function () {
+    return StudentSessions.find({ _id: Session.get("studentSessionId") }).fetch()[0];
+};
+
+Template.queueModalBody.stateWaiting = function () {
+    return Template.queueModalBody.studentSession().state == STUDENT_SESSION_STATE.WAITING;
+};
+
+Template.queueModalBody.studentsInFront = function () {
+    var studentsInFront = StudentQueue.find({ queueNr: { $lt: Session.get("queueNr") }}).count();
+    return studentsInFront;
+};
+
+Template.queueModalBody.stateReady = function () {
+    return Template.queueModalBody.studentSession().state == STUDENT_SESSION_STATE.READY;
+};
+
+Template.queueModalBody.events({
+    'click button#getHelp' : function () {
+        window.open(this.videoConferenceUrl);
+        StudentSessions.update(
+            { _id: Session.get("studentSessionId") },
+            { $set: { state: STUDENT_SESSION_STATE.GETTING_HELP } });
+    }
+});
