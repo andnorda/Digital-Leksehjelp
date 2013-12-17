92aee062731fe7fda5b18ed517fe44833cf633cc
Martin A. Midtsund <martin@iterate.no>
2013-12-17 12:18:34 +0100

    Refactor code and remove console.logs

--- client/views/studentSessions.js
+++ client/views/studentSessions.js
@@ -8,13 +8,11 @@ Template.studentSessionsTable.studentSessions = function () {
 */
 Template.studentSessionItem.events({
     'click .startTutoring' : function () {
-        console.log("TUTOR = ");
-        console.log(Meteor.user().profile);
         window.open(this.videoConferenceUrl, '_blank');
-        StudentSessions.update({ _id: this._id }, { $set: { state: STUDENT_SESSION_STATE.READY, tutor: Meteor.user().profile.firstName } });
-        if (typeof console !== 'undefined') {
-            console.log("You pressed the startTutoring button");
-        };
+        StudentSessions.update(
+            { _id: this._id },
+            { $set: { state: STUDENT_SESSION_STATE.READY, tutor: Meteor.user().profile.firstName }
+        });
     },
 
     'click .deleteSession' : function () {
