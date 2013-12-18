fd05d418b4560a5768178378b10781ce5e70d37f
Martin A. Midtsund <martin@iterate.no>
2013-12-18 17:04:06 +0100

    Add separation of own subjects and other subjects when helping students

--- client/views/studentSessions.js
+++ client/views/studentSessions.js
@@ -1,12 +1,26 @@
-// START STUDENTSSESSIONSTABLE
-Template.studentSessionsTable.studentSessions = function () {
-    return StudentSessions.find();
+Template.studentSessionsTable.myStudentSessions = function () {
+    var query = {
+        "$or": []
+    };
+    var subjects = Meteor.user().profile.subjects;
+    for (var i = 0; i < subjects.length; i++) {
+        query.$or.push({ "subject": subjects[i] });
+    };
+    return StudentSessions.find(query);
 };
 
-/**
-* studentSessionItem
-*/
-Template.studentSessionItem.events({
+Template.studentSessionsTable.otherStudentSessions = function () {
+    var query = {
+        "$and": []
+    };
+    var subjects = Meteor.user().profile.subjects;
+    for (var i = 0; i < subjects.length; i++) {
+        query.$and.push({ "subject": { $ne: subjects[i] } });
+    };
+    return StudentSessions.find(query);
+};
+
+Template.studentSessionRow.events({
     'click .startTutoring' : function () {
         window.open(this.videoConferenceUrl, '_blank');
         StudentSessions.update(
