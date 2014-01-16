53c58b659493494798028085d0bcd0f4b4f0295a
Martin A. Midtsund <martin@iterate.no>
2014-01-16 18:37:15 +0100

    Need to publish role, so the volunteers are aware of their own role

--- server/publish.js
+++ server/publish.js
@@ -23,6 +23,8 @@ Meteor.publish("loggedInUsers", function () {
                 'profile.pictureUrl': 1,
                 'profile.firstName': 1,
                 'profile.subjects': 1,
+                //TODO(martin): These last two fields could be more restricted
+                'profile.role': 1,
                 'services.resume.loginTokens': 1
             }
         });
