3476c4230c300034563bff1434066e1de0b58119
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2014-11-14 11:16:36 +0100

    Bug fix. Naming conflict

--- client/views/queueModal.js
+++ client/views/queueModal.js
@@ -5,7 +5,7 @@ Template.queueModal.rendered = function () {
     if (data && data.events) {
         if (!data.events.hidden) {
             $('#queueModal').on('hidden.bs.modal', function () {
-                var session = studentSession();
+                var session = findStudentSession();
                 if (session && (session.state !== STUDENT_SESSION_STATE.GETTING_HELP)) {
                     mixpanel.track("Forlot leksehjelp-kø",
                         {
@@ -23,27 +23,27 @@ Template.queueModal.rendered = function () {
     }
 };
 
-var studentSession = function () {
+var findStudentSession = function () {
     return StudentSessions.findOne({ _id: Session.get("studentSessionId") });
 }
 
 Template.queueModalBody.helpers({
     studentSession: function () {
-        return studentSession();
+        return findStudentSession();
     },
     stateWaiting: function () {
-        var studentSession = studentSession();
+        var studentSession = findStudentSession();
         return studentSession && studentSession.state == STUDENT_SESSION_STATE.WAITING;
     },
     studentsInFront: function () {
-        var studentSession = studentSession();
+        var studentSession = findStudentSession();
         return StudentQueue.find({ $and: [
                 { queueNr: { $lt: studentSession.queueNr } },
                 { subject: studentSession.subject }
             ]}).count();
     },
     stateReady: function () {
-        var studentSession = studentSession();
+        var studentSession = findStudentSession();
         var stateReady = studentSession && studentSession.state == STUDENT_SESSION_STATE.READY;
         if (stateReady) {
             flashTitle("Leksehjelpen er klar!", 20);
