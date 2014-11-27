cfd1cdb61109058828edb3a40495cf93c586bb05
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2014-11-27 10:59:00 +0100

    Moved template helper to seperate file
      * Added new helpers

--- client/application.js
+++ client/application.js
@@ -40,58 +40,6 @@ Meteor.Spinner.options = {
     top: '3'
 };
 
-Template.registerHelper('isGreaterThanZero', function(value) {
-  if(value > 0) {
-    return true;
-  }
-  return false
-});
-
-Template.registerHelper('not', function(value) {
-    return !value;
-});
-
-Template.registerHelper('globalRoles', function(block) {
-    return ROLES;
-});
-
-Template.registerHelper('optionsSelected', function(values, defaultValue) {
-    var buffer = "";
-    if (Array.isArray(values)) {
-        // TODO(martin): If needed, treat as array.
-    } else {
-        // Treat as object
-        for (var value in values) {
-            var option = '';
-            if (values[value] === defaultValue) {
-                buffer += '<option selected>' + values[value] + '</option>';
-            } else {
-                buffer += '<option>' + values[value] + '</option>';
-            };
-        }
-    }
-    return new Spacebars.SafeString(buffer);
-});
-
-Template.registerHelper('transformNewline', function(text) {
-    return new Spacebars.SafeString(text.replace(/\n/g, "<br>"));
-});
-
-Template.registerHelper('serviceIsOpen', function () {
-    var serviceStatusArray = Config.find({ name: "serviceStatus" }).fetch();
-        if (serviceStatusArray.length > 0) {
-            return serviceStatusArray[0].open;
-        }
-        return false;
-    }
-);
-
-Template.registerHelper('fromNow', function(date) {
-    if (date) {
-        return moment(date).fromNow();
-    }
-});
-
 (function () {
     var original = document.title;
     var timeout;
--- client/views/student/showAnswer.js
+++
@@ -1,6 +0,0 @@
-Template.showAnswer.helpers({
-    subjectName: function(subjectId) {
-        var subject = Subjects.findOne({ _id: subjectId });
-        return (subject) ? subject.name : "Ukjent fag";
-    }
-});
