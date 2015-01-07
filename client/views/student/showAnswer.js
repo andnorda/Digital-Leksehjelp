23e189df881157d9ae27b51b9c0717fe408c8bd2
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2015-01-07 11:19:17 +0100

    Show warning when question is not publicly available

--- client/views/student/showAnswer.js
+++ client/views/student/showAnswer.js
@@ -1,3 +1,11 @@
 Template.showAnswer.rendered = function() {
     window.scrollTo(0, 0);
 }
+
+Template.showAnswer.helpers({
+    showNonpublicQuestionWarning: function () {
+        if (Meteor.user() && this.question) {
+            return !this.publishedBy || !this.verifiedBy;
+        }
+    }
+});
