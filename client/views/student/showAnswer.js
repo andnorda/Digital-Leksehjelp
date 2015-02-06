c2eb210d54903717f18cc120262bead7f065d379
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2015-02-06 16:23:20 +0100

    Volunteers can verify answers from preview

--- client/views/student/showAnswer.js
+++ client/views/student/showAnswer.js
@@ -10,5 +10,27 @@ Template.showAnswer.helpers({
         if (Meteor.user() && this.question) {
             return !this.publishedBy || !this.verifiedBy;
         }
+    },
+    canBeVerified: function() {
+        if (Meteor.user() && this.question) {
+            return !this.verifiedBy;
+        }
+        return false;
+    }
+});
+
+Template.showAnswer.events({
+    'click .verify-answer': function(event, template) {
+        if (Meteor.user()) {
+            Meteor.call('verifyAnswer',
+            {
+                questionId: this._id
+            },
+            function (error) {
+                if (error) {
+                    FlashMessages.sendError(error.message);
+                }
+            });
+        }
     }
 });
