02ebc2d4d7140a470973919d2bb7edb7000957c2
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2016-03-16 22:42:03 +0100

    Add rapid enage survey. Triggers when a student starts a videochat

--- client/views/student/modals/queueModal.js
+++ client/views/student/modals/queueModal.js
@@ -1,3 +1,14 @@
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 Template.queueModal.rendered = function () {
     var elem = $('#queueModal')[0];
     var data = $.hasData(elem) && $._data(elem);
@@ -77,6 +88,11 @@ Template.queueModalBody.events({
             sessionId: Session.get("studentSessionId"),
             state: STUDENT_SESSION_STATE.GETTING_HELP
         });
+
+        // need to delete RapidEngage cookies to make sure the the survey pops up
+        // (can possibly leave the cookie after testing is done)
+        deleteAllCookies();
+        startRapidEngageSurvey();
 
         cancelFlashTitle();
     }
