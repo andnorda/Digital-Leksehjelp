55b1e2249562f51dadf62ae8a1509bee47d3177f
Andreas Nordahl <andnorda@gmail.com>
2018-03-25 18:17:48 +0200

    move loggedInHeader helpers to correct file

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,3 @@
-import { StudentSessions } from '/imports/api/studentSessions/studentSessions.js';
-
-import { STUDENT_SESSION_STATE } from '/imports/constants';
-
-import { ROLES } from '/imports/constants';
-
 import './routes.js';
 import './templateHelpers.js';
 import './flashTitle.js';
@@ -130,39 +124,4 @@ function initGetSiteControl() {
 Meteor.startup(function() {
     initMixpanel();
     initGetSiteControl();
-});
-
-Template.loggedInHeader.helpers({
-    currentUserEmail: function() {
-        return Meteor.user().username;
-    },
-    isActiveTab: function(route) {
-        if (Router.current().route.getName() === route) {
-            return 'active';
-        }
-    },
-    isAdmin: function() {
-        return Meteor.user().profile.role === ROLES.ADMIN;
-    },
-    isVideohelper: function() {
-        var user = Meteor.user();
-        return user.profile.role === ROLES.ADMIN || user.profile.allowVideohelp;
-    },
-    numberOfStudentsWaitingInQueue: function() {
-        var number = StudentSessions.find({
-            state: STUDENT_SESSION_STATE.WAITING
-        }).count();
-
-        if (number > 0) {
-            return ' (' + number + ' i kø)';
-        }
-    }
-});
-
-Template.login.events({
-    click: function(event) {
-        if (event.currentTarget.id === 'signup-link') {
-            alert('Vennligst snakk med Røde Kors for å få en bruker her.');
-        }
-    }
 });
--- imports/ui/components/loggedInHeader/loggedInHeader.js
+++ imports/ui/components/loggedInHeader/loggedInHeader.js
@@ -1,1 +1,31 @@
+import { StudentSessions } from '/imports/api/studentSessions/studentSessions.js';
+import { ROLES, STUDENT_SESSION_STATE } from '/imports/constants';
+
 import './loggedInHeader.html';
+
+Template.loggedInHeader.helpers({
+    currentUserEmail: function() {
+        return Meteor.user().username;
+    },
+    isActiveTab: function(route) {
+        if (Router.current().route.getName() === route) {
+            return 'active';
+        }
+    },
+    isAdmin: function() {
+        return Meteor.user().profile.role === ROLES.ADMIN;
+    },
+    isVideohelper: function() {
+        var user = Meteor.user();
+        return user.profile.role === ROLES.ADMIN || user.profile.allowVideohelp;
+    },
+    numberOfStudentsWaitingInQueue: function() {
+        var number = StudentSessions.find({
+            state: STUDENT_SESSION_STATE.WAITING
+        }).count();
+
+        if (number > 0) {
+            return ' (' + number + ' i kø)';
+        }
+    }
+});
