9c9da92f78ccb0975f921cdd5ec65ed5f56dba44
Andreas Nordahl <andnorda@gmail.com>
2018-06-11 19:09:13 +0200

    delete some old modal code

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,5 +1,1 @@
 import './body.html';
-
-import '../../components/queueModal/queueModal.js';
-import '../../components/notSupportedModal/notSupportedModal.js';
-import '../../components/moreInfoModal/moreInfoModal.js';
