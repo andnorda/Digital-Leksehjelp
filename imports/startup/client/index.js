0fb107270254d49f4c95b1675820697295d1ec7e
Andreas Nordahl <andnorda@gmail.com>
2020-03-20 09:27:30 +0100

    use Meteor.isProduction to get env

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -11,45 +11,27 @@ Meteor.Spinner.options = {
 };
 
 function initMixpanel() {
-    let environment;
-    Meteor.call('getEnvironment', function(error, result) {
-        if (error) {
-            environment = 'development';
-        } else {
-            environment = result;
-
-            if (environment === 'development') {
-                mixpanel.init('2ba4e2b936870c8ee8537a3d9d76dea7');
-            } else if (environment === 'production') {
-                mixpanel.init('f84c7b2904d686b254dc2d1d032d6e56');
-            }
-
-            mixpanel.track('Antall besøkende', {
-                url: window.location.pathname
-            });
-        }
+    mixpanel.init(Meteor.isProduction
+        ? 'f84c7b2904d686b254dc2d1d032d6e56'
+        : '2ba4e2b936870c8ee8537a3d9d76dea7');
+    mixpanel.track('Antall besøkende', {
+        url: window.location.pathname
     });
 }
 
 function initGetSiteControl() {
     window.showSurvey = function() {
-        Meteor.call('getEnvironment', function(error, result) {
-            let getSiteControlSurveyUrl =
-                '//widgets.getsitecontrol.com/130166/script.js'; // development
-            if (!error && result === 'production') {
-                getSiteControlSurveyUrl =
-                    '//widgets.getsitecontrol.com/43318/script.js';
-            }
-
-            (function(w, i, d, g, e, t, s) {
-                w[d] = w[d] || [];
-                t = i.createElement(g);
-                t.async = 1;
-                t.src = e;
-                s = i.getElementsByTagName(g)[0];
-                s.parentNode.insertBefore(t, s);
-            })(window, document, '_gscq', 'script', getSiteControlSurveyUrl);
-        });
+        const getSiteControlSurveyUrl = Meteor.isProduction
+            ? '//widgets.getsitecontrol.com/43318/script.js'
+            : '//widgets.getsitecontrol.com/130166/script.js';
+        (function(w, i, d, g, e, t, s) {
+            w[d] = w[d] || [];
+            t = i.createElement(g);
+            t.async = 1;
+            t.src = e;
+            s = i.getElementsByTagName(g)[0];
+            s.parentNode.insertBefore(t, s);
+        })(window, document, '_gscq', 'script', getSiteControlSurveyUrl);
     };
 }
 
