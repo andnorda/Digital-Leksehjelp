9cf7796c321347b792ac6850d4e1a69e5176a98b
Andreas Nordahl <andnorda@gmail.com>
2021-04-11 19:09:26 +0200

    remove unused 'users.loggedIn'

--- imports/ui/components/helpTopicSelector/helpTopicSelector.js
+++ imports/ui/components/helpTopicSelector/helpTopicSelector.js
@@ -1,14 +1,11 @@
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { HelpTopics } from '/imports/api/helpTopics/helpTopics.js';
-import { Config } from '/imports/api/config/config.js';
 
 import './helpTopicSelector.html';
 
-Template.helpTopicSelector.onCreated(function() {
+Template.helpTopicSelector.onCreated(function () {
     this.autorun(() => {
         this.subscribe('helpTopics');
-        this.subscribe('users.loggedIn');
     });
 });
 
